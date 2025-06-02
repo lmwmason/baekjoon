dbbf7ec5b4a474bc719624e78d8fdd45ebb2a8ed
이무원 <149868756+lmwmason@users.noreply.github.com>
2025-06-02 12:04:39 +0900

    Create 2839.py

---
+++ 2839.py
@@ -0,0 +1,13 @@
+N = int(input())
+cnt = 0
+
+while(True) :
+    if(N%5==0) :
+        cnt+=N//5
+        break
+    N-=3
+    cnt+=1
+    if(N<=0) :
+        break
+if(N<0) : print(-1)
+else : print(cnt)
